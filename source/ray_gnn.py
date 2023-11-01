c60151dc7c75c32693d08f93e3dbec20b12f6d94
Bruce Xi <91307696+brucexi999@users.noreply.github.com>
2023-11-01 11:08:53 -0700

    Update ray_gnn.py

--- source/ray_gnn.py
+++ source/ray_gnn.py
@@ -5,21 +5,20 @@ import torch.nn as nn
 import gymnasium as gym
 from ray.rllib.models.modelv2 import ModelV2
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
-from ray.rllib.policy.rnn_sequencing import add_time_dimension
 from ray.rllib.utils.annotations import override
 from GNN import MPNN
 
 
 # Custom ray-compatible GNN-based model
 class MPNNModel(TorchModelV2, nn.Module):
-    def __init(self, obs_space: gym.spaces.Space, action_space: gym.spaces.Space, num_outputs: int, model_config: dict, name: str):
+    def __init__(self, obs_space: gym.spaces.Space, action_space: gym.spaces.Space, num_outputs: int, model_config: dict, name: str):
         TorchModelV2.__init__(self, obs_space, action_space, num_outputs, model_config, name)
         nn.Module.__init__(self)
 
         # General specs
         self.n_agents = 1
         self.node_features_dim = model_config['node_features_dim']
-        self.edge_features_dim = model_config['edge_features_dim']
+        self.edge_features_dim = model_config['edge_features_dim']  # Not used
         self.action_space_dim = model_config['action_space_dim']
         self._cur_values = None
         # TODO: Optimize above code & add more!
@@ -42,15 +41,15 @@ class MPNNModel(TorchModelV2, nn.Module):
         obs = input_dict["obs"]
 
         # Decompose the observation into its different matrices
-        node_features = obs["node_features"]
-        edge_features = obs["edge_features"]
-        adjacency_matrix = obs["adjacency_matrix"]
+        node_features = obs["node_feature_mat"]
+        edge_features = obs["edge_feature_mat"]
+        adjacency_matrix = obs["adj_max"]
 
         # Calculate new policy & values
-        action_probabilities = self.policy_nn(node_features, edge_features, adjacency_matrix)
+        policy_logits = self.policy_nn(node_features, edge_features, adjacency_matrix)
         self._cur_values = self.value_nn(node_features, edge_features, adjacency_matrix)
 
-        return action_probabilities, state
+        return policy_logits, state
 
     @override(ModelV2)
     def value_function(self):
